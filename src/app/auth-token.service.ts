a37ce3cb2df454ba5e21e461f672c277d24692b6
Olingo <195367@ecam.be>
2023-05-02 22:13:22 +0200

    e

---
+++ src/app/auth-token.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthTokenService {
+
+  constructor() { }
+
+  private token: string = "";
+
+  setToken(token: string) {
+    console.log("setToken");
+    console.log(token);
+    this.token = token;
+  }
+
+  getToken() {
+    console.log("getToken");
+    return this.token;
+  }
+}
